8038b022625a5781192d50967caef40b67b37543
rbeze <rbeze09@gmail.com>
2024-03-13 16:59:14 -0300

    implemented function for renaming images based on index

--- img_download.py
+++ img_download.py
@@ -1,4 +1,16 @@
 import requests
+import os
+
+def name_img():
+    img_db = os.listdir("/home/rbeze/Desktop/gemini/img")
+    print(img_db)
+    if len(img_db) == 0:
+        img_name = "img_0"
+    else:
+
+        img_number = img_db[0].split("_")[1].split(".")[0]
+        img_name = f"img_{int(img_number) + 1}" 
+        print(img_name)
 
 def download_image(url, filename):
     response = requests.get(url)
@@ -12,4 +24,5 @@ def download_image(url, filename):
 # Exemplo de uso:
 url = "https://t.ctcdn.com.br/lvns56iaSMyHvyTur4JeYS_NYeY=/i606944.png"
 nome_arquivo = "imagem.jpg"
-download_image(url, nome_arquivo)
+#download_image(url, nome_arquivo)
+name_img()
